6d7b1835337a79b0a41e0405348d6d75e57c2619
sebuelias <sebuelias27@gmail.com>
2021-03-19 00:26:03 +0530

    removed all comments, since netlify deploy failing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from './components/HomeScreen/HomeScreen';
-import GameScreen from './components/GameScreen/GameScreen';
-import EndScreen from './components/EndScreen/EndScreen';
 
 function App() {
   return (
     <div className='App'>
       <HomeScreen />
-      {/* <GameScreen /> */}
-      {/* <EndScreen /> */}
     </div>
   );
 }
